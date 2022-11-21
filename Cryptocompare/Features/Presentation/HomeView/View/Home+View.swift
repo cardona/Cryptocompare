f8275f048c2c04ef21191a60298993e92b3cee63
Oscar Cardona <ocardona@gmail.com>
2022-11-21 21:07:55 +0100

    Add base files

---
+++ Cryptocompare/Features/Presentation/HomeView/View/Home+View.swift
@@ -0,0 +1,14 @@
+//
+//  Home+View.swift
+//  Cryptocompare
+//
+//  Created by oscar on 21/11/22.
+//
+
+import Foundation
+import UIKit
+
+final class HomeView: UIViewController {
+    weak var coordinator: MainCoordinator?
+    
+}
