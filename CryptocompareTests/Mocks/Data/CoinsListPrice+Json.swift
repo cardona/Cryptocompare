a6cb925add31098abdd8ea157c4aac65d9a64de3
Oscar Cardona <ocardona@gmail.com>
2022-11-22 22:26:12 +0100

    Add Coins Price Mock

--- CryptocompareTests/Mocks/Data/CoinsListPrice+Json.swift
+++ CryptocompareTests/Mocks/Data/CoinsListPrice+Json.swift
@@ -5,4 +5,28 @@
 //  Created by oscar on 22/11/22.
 //
 
-import Foundation
+import XCTest
+
+final class CoinsListPriceMock: XCTestCase {
+    static func makeJsonMock() -> Data {
+        return Data("""
+{
+   "ETH":{
+      "BTC":0.06986,
+      "USD":1123.02,
+      "EUR":1090.61
+   },
+   "CRAIG":{
+      "BTC":1.9e-7,
+      "USD":0.003054,
+      "EUR":0.002967
+   },
+   "DASH":{
+      "BTC":0.002275,
+      "USD":36.6,
+      "EUR":35.5
+   }
+}
+""".utf8)
+    }
+}
---
+++ CryptocompareTests/Mocks/Data/CoinsListRepository+Mock.swift
@@ -0,0 +1,28 @@
+//
+//  CoinsListRepository+Mock.swift
+//  CryptocompareTests
+//
+//  Created by oscar on 22/11/22.
+//
+
+import XCTest
+import SKRools
+@testable import Cryptocompare
+
+
+extension CoinsListUseCaseTest {
+    final class CoinsListRepositorySuccessMock: CoinsListRepository {
+        func request(parameters: CoinsListRepositoryParameters, completion: @escaping (Result<CoinsListDecodable, DataTransferError>) -> Void) -> SKRools.Cancellable? {
+
+            let decoder = JSONDecoder()
+            let json = CoinsListJsonMock.makeJsonMock()
+            if let decodable = try? decoder.decode(CoinsListDecodable.self, from: json) {
+                completion(.success(decodable))
+            } else {
+                XCTFail("Bad Coins JSON Mockup")
+            }
+
+            return nil
+        }
+    }
+}
