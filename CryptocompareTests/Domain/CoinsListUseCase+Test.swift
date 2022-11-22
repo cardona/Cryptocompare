b9ece951266a876d77a73be02f728d782c6e9567
Oscar Cardona <ocardona@gmail.com>
2022-11-22 23:55:44 +0100

    Implement first test

--- CryptocompareTests/Domain/CoinsListUseCase+Test.swift
+++ CryptocompareTests/Domain/CoinsListUseCase+Test.swift
@@ -17,21 +17,56 @@ final class CoinsListUseCaseTest: XCTestCase {
         // GIVEN
 
         // create mocked success repository
-        let repository = CoinsListRepositorySuccessMock()
+        let coinsRepository = CoinsListRepositorySuccessMock()
+        // create mocked success repository
+        let priceRepository = CoinsListPriceRepositorySuccessMock()
+
         // Inject mocked mockup into useCase
-        let useCase: CoinsListUseCase = DefaultCoinsListUseCase(repository: repository)
+        let useCase: CoinsListUseCase = DefaultCoinsListUseCase(coinsRepository: coinsRepository, priceRepository: priceRepository)
 
         // WHEN
         useCase.execute(completion: { result in
             // THEN
             switch result {
             case .success(let entity):
-                break
+                XCTAssertEqual(entity.count, 5)
+                XCTAssertEqual(entity.first?.fullName, "BitcoinDark (BTCD)")
+                XCTAssertEqual(entity.first?.symbol, "BTCD")
+                XCTAssertEqual(entity.first?.price, 48.45)
+                XCTAssertEqual(entity.first?.imageUrl, "/media/19630/btcd_1.png")
+                XCTAssertEqual(entity.first?.identifier, "4400")
+                XCTAssertEqual(entity[4].symbol, "PRC")
+                XCTAssertEqual(entity[4].price, 0.01079)
             case .failure(let error):
-                break
+                XCTAssertNil(error)
             }
             expt.fulfill()
         })
         wait(for: [expt], timeout: 10)
+    }
+
+    func testCoinsListUseCaseWhenEmptyDataSuccess() {
+        let expt = expectation(description: "Array without Coins is returned")
+
+    }
+
+    func testCoinsListUseCaseWhen403Failure() {
+        let expt = expectation(description: "A controlled error")
+
+    }
+
+    func testCoinsListUseCaseWhen500Failure() {
+        let expt = expectation(description: "A controlled error")
+
+    }
+
+    func testCoinsListUseCaseWhenWithoutInternetFailure() {
+        let expt = expectation(description: "A controlled error")
+
+    }
+
+    func testCoinsListUseCaseWhenWithoutInternetSuccess() {
+        let expt = expectation(description: "A controlled error")
+
     }
 }
