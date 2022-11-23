1d5b564c9ac4c067eb1f8f60d4f77df31767726d
Oscar Cardona <ocardona@gmail.com>
2022-11-23 22:49:23 +0100

    Add CoinDetailRepository Mocked

---
+++ CryptocompareTests/Domain/CoinDetailUseCase+Test.swift
@@ -0,0 +1,17 @@
+//
+//  CoinDetailUseCase+Test.swift
+//  CryptocompareTests
+//
+//  Created by oscar on 23/11/22.
+//
+
+import Foundation
+import XCTest
+@testable import Cryptocompare
+
+final class CoinDetailUseCaseTest: XCTestCase {
+
+    func testCoinDetailUseCaseWhenSuccess() {
+
+    }
+}
---
+++ CryptocompareTests/Mocks/Data/CoinDetailRepository+Mock.swift
@@ -0,0 +1,27 @@
+//
+//  CoinDetailRepository+Mock.swift
+//  CryptocompareTests
+//
+//  Created by oscar on 23/11/22.
+//
+
+import XCTest
+import SKRools
+@testable import Cryptocompare
+
+
+extension CoinDetailUseCaseTest {
+    final class CoinDetailRepositorySuccessMock: CoinDetailRepository {
+        func request(parameters: Cryptocompare.CoinDetailRepositoryParameters, completion: @escaping (Result<[String : String], SKRools.DataTransferError>) -> Void) -> SKRools.Cancellable? {
+            let decoder = JSONDecoder()
+            let json = CoinDetailsJsonMock.makeJsonMock()
+            if let decodable = try? decoder.decode([String : String].self, from: json) {
+                completion(.success(decodable))
+            } else {
+                XCTFail("Bad Coin Detail JSON Mockup")
+            }
+
+            return nil
+        }
+    }
+}
