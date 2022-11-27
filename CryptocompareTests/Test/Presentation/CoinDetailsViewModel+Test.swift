c06e8593a507ca7c12a81e10314851c2a22ec376
Oscar Cardona <ocardona@gmail.com>
2022-11-27 15:51:34 +0100

    Implement test for conversion

--- CryptocompareTests/Test/Presentation/CoinDetailsViewModel+Test.swift
+++ CryptocompareTests/Test/Presentation/CoinDetailsViewModel+Test.swift
@@ -47,5 +47,44 @@ final class CoinDetailsViewModelTest: XCTestCase {
 
         wait(for: [expt], timeout: 10)
     }
+
+    func testCoinDetailsViewModelWhenConvertCoinSuccess() {
+        let expt = expectation(description: "Data is converted to use in view")
+
+        // create mocked success repository
+        let coinDetailsRepository = CoinDetailRepositorySuccessMock()
+        // create mocked success config
+        let configUseCase = ConfigUseCaseSuccessMock()
+        // create mocked success coreData
+        let coinCoreData = CoinDetailsCoreDataSuccessMock()
+
+        // create mocked success useCase
+        let useCase: CoinDetailUseCase = DefaultCoinDetailUseCase(repository: coinDetailsRepository,
+                                                                  useCase: configUseCase,
+                                                                  coreData: coinCoreData)
+
+        // Inject mocked mockup into ViewModel
+        let viewModel: CoinDetailsViewModel = DefaultCoinDetailsViewModel(useCase: useCase, config: configUseCase)
+
+        viewModel.model.bind(listener: { model in
+            guard let _ = model else { return }
+            viewModel.convertCoin(quantity: "687")
+        })
+
+        viewModel.result.bind(listener: { model in
+            guard let model = model else { return }
+            XCTAssertEqual(model, "0.611 ETH")
+
+            expt.fulfill()
+        })
+
+        viewModel.error.bind(listener: { error in
+            XCTAssertNil(error)
+        })
+
+        viewModel.coinDetails(symbol: "ETH")
+
+        wait(for: [expt], timeout: 10)
+    }
 }
 
